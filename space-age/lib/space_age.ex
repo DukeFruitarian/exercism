e259dc85ebae28ec337b10b3372658ce15594eb2
Duke <dukefruitarian@gmail.com>
2019-08-01 09:56:20 +0300

    Implement 2 quizes

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -9,11 +9,23 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
 
+  @earth_relative_years %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(planet, seconds) do
-  end
+  def age_on(planet, seconds), do: earth_years(seconds) / @earth_relative_years[planet]
+
+  defp earth_years(seconds), do: seconds / 31_557_600
 end
