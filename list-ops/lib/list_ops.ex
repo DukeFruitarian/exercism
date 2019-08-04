a9668dde07599cbe29aeb4f177ef3e8e9fbc2994
Duke <dukefruitarian@gmail.com>
2019-08-04 14:11:16 +0300

    Implement list ops

--- list-ops/lib/list_ops.ex
+++ list-ops/lib/list_ops.ex
@@ -6,31 +6,50 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-  end
+  def count(l), do: do_count(l, 0)
+
+  defp do_count([], size), do: size
+  defp do_count([_ | t], size), do: do_count(t, size + 1)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-  end
+  def reverse(l), do: do_reverse(l, [])
+
+  defp do_reverse([], reversed), do: reversed
+  defp do_reverse([h | t], reversed), do: do_reverse(t, [h | reversed])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-  end
+  def map(l, f), do: do_map(l, f, [])
+
+  def do_map([], _, result), do: reverse(result)
+  def do_map([h | t], f, result), do: do_map(t, f, [f.(h) | result])
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter(l, f), do: do_filter(l, f, [])
+
+  def do_filter([], _, result), do: reverse(result)
+
+  def do_filter([h | t], f, result) do
+    case f.(h) do
+      false -> do_filter(t, f, result)
+      true -> do_filter(t, f, [h | result])
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
-  end
+
+  def reduce([], acc, _), do: acc
+  def reduce([h | t], acc, f), do: reduce(t, f.(h, acc), f)
 
   @spec append(list, list) :: list
-  def append(a, b) do
-  end
+  def append(a, b), do: do_append(reverse(a), b)
+
+  defp do_append([], b), do: b
+  defp do_append([h | t], b), do: do_append(t, [h | b])
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-  end
+  def concat(ll), do: do_concat(ll, [])
+
+  def do_concat([], result), do: reverse(result)
+  def do_concat([hl | tl], result), do: do_concat(tl, append(reverse(hl), result))
 end
