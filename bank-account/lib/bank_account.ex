57adf3b89fda53806cc2b8586022f008123ef034
Duke <dukefruitarian@gmail.com>
2019-08-05 17:53:55 +0300

    Implement bank account

--- bank-account/lib/bank_account.ex
+++ bank-account/lib/bank_account.ex
@@ -1,7 +1,10 @@
 defmodule BankAccount do
+  use GenServer
+
   @moduledoc """
   A bank account that supports access from multiple processes.
   """
+  defstruct balance: 0, opened: true
 
   @typedoc """
   An account handle.
@@ -13,6 +16,8 @@ defmodule BankAccount do
   """
   @spec open_bank() :: account
   def open_bank() do
+    {:ok, pid} = GenServer.start_link(__MODULE__, nil)
+    pid
   end
 
   @doc """
@@ -20,6 +25,7 @@ defmodule BankAccount do
   """
   @spec close_bank(account) :: none
   def close_bank(account) do
+    GenServer.cast(account, :close_account)
   end
 
   @doc """
@@ -27,6 +33,7 @@ defmodule BankAccount do
   """
   @spec balance(account) :: integer
   def balance(account) do
+    GenServer.call(account, :get_balance)
   end
 
   @doc """
@@ -34,5 +41,27 @@ defmodule BankAccount do
   """
   @spec update(account, integer) :: any
   def update(account, amount) do
+    GenServer.call(account, {:update_balance, amount})
+  end
+
+  # Server-side
+  def init(_), do: {:ok, %BankAccount{balance: 0, opened: true}}
+
+  def handle_call(:get_balance, _, state = %{opened: false}) do
+    {:reply, {:error, :account_closed}, state}
+  end
+
+  def handle_call(:get_balance, _, state = %{balance: balance}), do: {:reply, balance, state}
+
+  def handle_call({:update_balance, _}, _, state = %{opened: false}) do
+    {:reply, {:error, :account_closed}, state}
+  end
+
+  def handle_call({:update_balance, amount}, _, state = %{balance: balance}) do
+    {:reply, :ok, %{state | balance: balance + amount}}
+  end
+
+  def handle_cast(:close_account, state) do
+    {:noreply, %{state | opened: false}}
   end
 end
