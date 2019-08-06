a4adfa2864f11b246e2aa194c6b9ae9cc5a71b45
Duke <dukefruitarian@gmail.com>
2019-08-06 09:16:33 +0300

    Refacrtor frequency

--- parallel-letter-frequency/lib/frequency.ex
+++ parallel-letter-frequency/lib/frequency.ex
@@ -7,8 +7,6 @@ defmodule Frequency do
   The number of worker processes to use can be set with 'workers'.
   """
   @spec frequency([String.t()], pos_integer) :: map
-  def frequency([], _), do: %{}
-
   def frequency(texts, workers) do
     letters =
       Enum.join(texts)
@@ -18,27 +16,8 @@ defmodule Frequency do
 
     letters
     |> Enum.chunk_every(div(length(letters), workers) + 1)
-    |> Enum.map(&spawn_counter/1)
-    |> Enum.map(fn _ ->
-      receive do
-        {:calculation_result, value} ->
-          value
-      end
-    end)
-    |> compose_results
-  end
-
-  defp spawn_counter(list) do
-    parent = self()
-
-    spawn(fn ->
-      result = calculate_frequency(list, %{})
-      send(parent, {:calculation_result, result})
-    end)
-  end
-
-  defp compose_results(maps_list) do
-    Enum.reduce(maps_list, %{}, fn result_part, total_result ->
+    |> Task.async_stream(&calculate_frequency(&1, %{}))
+    |> Enum.reduce(%{}, fn {:ok, result_part}, total_result ->
       Map.merge(total_result, result_part, fn _, v1, v2 -> v1 + v2 end)
     end)
   end
