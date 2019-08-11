4c7e0be5f3691c5fc6c362c9df6e1452498ef799
Duke <dukefruitarian@gmail.com>
2019-08-11 12:53:49 +0300

    Implement isbn verifier

--- isbn-verifier/lib/isbn_verifier.ex
+++ isbn-verifier/lib/isbn_verifier.ex
@@ -11,7 +11,24 @@ defmodule IsbnVerifier do
       false
 
   """
+  @digits ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  defguard is_digit(grapheme) when grapheme in @digits
+
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
+    String.replace(isbn, ~r/(-)/, "")
+    |> valid?(10, 0)
   end
+
+  defp valid?(<<"X">>, 1, result), do: rem(result + 10, 11) == 0
+
+  defp valid?(<<digit::binary-1>>, 1, result) when is_digit(digit) do
+    rem(result + String.to_integer(digit), 11) == 0
+  end
+
+  defp valid?(<<digit::binary-1, rest::binary>>, multiplier, result) when is_digit(digit) do
+    valid?(rest, multiplier - 1, result + String.to_integer(digit) * multiplier)
+  end
+
+  defp valid?(_code, _multiplier, _result), do: false
 end
